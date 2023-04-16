543ab2ef588defb4540af0ba83e0d80364e1d3d8
Steve Magnuson <ag7gn.steve@gmail.com>
2023-04-16 14:08:32 -0700

    Version 2

--- rigctl_gui.sh
+++ rigctl_gui.sh
@@ -3,7 +3,7 @@
 # HEADER
 #================================================================
 #% SYNOPSIS
-#+   ${SCRIPT_NAME} [-hv]
+#+   ${SCRIPT_NAME} [-hv] [FILE]
 #%
 #% DESCRIPTION
 #%   This script provides a GUI to configure and operate rigctld.
@@ -12,9 +12,14 @@
 #%    -h, --help                  Print this help
 #%    -v, --version               Print script information
 #%
+#% OPTIONAL PARAMETERS
+#%    If supplied, this script will use the file path supplied
+#%    for the ARGS_CONFIG variable. That file will be used to 
+#%    to launch the rigctld systemd service. It will use a
+#%    default file if this parameter is not supplied.
 #================================================================
 #- IMPLEMENTATION
-#-    version         ${SCRIPT_NAME} 1.0.7
+#-    version         ${SCRIPT_NAME} 2.0.3
 #-    author          Steve Magnuson, AG7GN
 #-    license         CC-BY-SA Creative Commons License
 #-    script_id       0
@@ -25,6 +30,7 @@
 #     20200718 : Steve Magnuson : Delete unused function.
 #     20211129 : Steve Magnuson : Updated to suppor new locations
 #											 for pat confuguration
+#     20230318 : Steve Magnuson : Refactor for systemd
 # 
 #================================================================
 #  DEBUG OPTION
@@ -48,14 +54,22 @@ function TrapCleanup () {
 	do
 		kill $P >/dev/null 2>&1
 	done
-	rm -f $fpipe
-	#exec 4>&-
+	rm -f $TERM_PIPE
+	unset argModify
+	unset Message
+	unset rigctldStatus
+	unset restartRigctld
+	unset stopRigctld
+	unset RIGCTL_LOG
+	unset RIGS_FILE
+	unset ARGS_CONFIG
 }
 
 function SafeExit() {
 	TrapCleanup
    trap - INT TERM EXIT
-   exit 0
+	[[ -d "${TMPDIR}" ]] && rm -rf "${TMPDIR}/" 2>/dev/null
+   exit ${1:-0}
 }
 
 function ScriptInfo() { 
@@ -81,50 +95,13 @@ function Die () {
 	SafeExit
 }
 
-function runFind () {
-	echo "2:@disable@"
-	echo -e '\f' >> "$fpipe"
-	while IFS= read -r LINE
-	do
-		N="$(trim "${LINE:2:4}")"
-		M="$(trim "${LINE:8:23}")"
-  		O="$(trim "${LINE:31:24}")"
-		echo -e "\n$N\n$M\n$O" >> "$fpipe"
-	done < <($(command -v rigctl) -l | grep -v "^ Rig" | sed -e 's/&/&#x26;/g' | grep -i "$1")
-	echo "2:$find_cmd"
+function Message () {
+	# Data piped to this function is sent to a stdout, prepended by  
+	# a time stamp
+   TIME_FORMAT="%Y/%m/%d %H:%M:%S"
+   echo "$1" | stdbuf -oL ts "${TIME_FORMAT}" >> $RIGCTL_LOG
 }
-export -f runFind
-
-function viewDeleteAliases () {
-	# Load existing aliases
-	while true
-	do
-		# Read aliases from $PAT_CONFIG
-		ALIASES="$(jq -r .connect_aliases $PAT_CONFIG | egrep -v "telnet|{|}" | \
-				  sed 's/^ /FALSE|/' | tr -d ' ",' | sed 's/:/|/1' | tr '|' '\n')"
-		RESULT="$(yad --title="View/remove pat aliases" --list --mouse --borders=10 \
-				--height=400 --width=400 --text-align=center \
-				--text "<b>Your current pat connection aliases are listed below.</b>\n \
-Check the ones you want to remove.\n" \
-				--checklist --grid-lines=hor --auto-kill --column="Pick" --column="Call" --column="Connect URI" \
-				<<< "$ALIASES" --buttons-layout=center --button="Exit":1 --button="Refresh list":0 --button="Remove selected aliases":0)"
-		if [[ $? == 0 ]]
-		then # Refresh or removal requested
-      	while IFS="|" read -r CHK KEY VALUE REMAINDER
-			do # read each checked alias
-				if [[ $CHK == "TRUE" ]]
-				then # Remove alias
-					cat $PAT_CONFIG | jq --arg K "$KEY" --arg V "$VALUE" \
-						'(.connect_aliases | select(.[$K] == $V)) |= del (.[$K])' | sponge $PAT_CONFIG
-				fi
-			done <<< "$RESULT"	
-		else # User cancelled
-			break
-		fi
-	done
-	exit 0
-}
-export -f viewDeleteAliases
+export -f Message
 
 function trim() {
   # Trims leading and trailing white space from a string
@@ -133,53 +110,126 @@ function trim() {
   until s2="${s%[[:space:]]}"; [ "$s2" = "$s" ]; do s="$s2"; done
   echo "$s"
 }
-export -f trim
 
-function getRig() {
-	LINE="$($(command -v rigctl) -l | grep -v "^ Rig" | egrep "^[[:space:]]*$1")"
-	N="$(trim "${LINE:2:4}")"
-	M="$(trim "${LINE:8:23}")"
-  	O="$(trim "${LINE:31:24}")"
-	echo "$M $O"
+function argModify () {
+   local ARG=$(echo $@ | cut -d '=' -f1)
+   grep -v "^${ARG}" $ARGS_CONFIG | sponge $ARGS_CONFIG
+   cat >> $ARGS_CONFIG <<EOF
+$@
+EOF
 }
+export -f argModify
 
-function setrigctldDefaults () {
-   declare -gA D
-   D[1]="1|Hamlib|Dummy"  # Rig ID
-   D[2]="Not Applicable" # Serial Port
-   D[3]="Not Applicable" # Serial Port Speed
+function rigctldStatus() {
+	if systemctl --user --quiet is-active $(systemd-escape --template rigctld@.service "$ARGS_CONFIG")
+	then
+		local PROCESS="$(systemd-cgls -l --user $(systemd-escape \
+		--template rigctld@.service "$ARGS_CONFIG") | grep "rigctld -")"
+		PROCESS="${PROCESS:2}"
+		local PID=$(echo "$PROCESS" | awk '{print $1}')
+		local CMD="$(ps -o args= $PID)"
+		local TOKEN="${CMD##*-m }"
+		local RIG_NUM="${TOKEN%% *}"
+		source $RIGS_FILE
+		local MAKE_MODEL="${RIGS_ARRAY[$RIG_NUM]}"
+		TOKEN="${CMD##*-r }"
+		[[ -n $TOKEN ]] && local DEVICE="${TOKEN%% *}"
+		TOKEN="${CMD##*-s }"
+		[[ -n $TOKEN ]] && local SPEED="${TOKEN%% *}"
+		case $RIG_NUM in
+			1|2|4|6)
+				Message "rigctld is running. Rig # ${RIG_NUM} $MAKE_MODEL"
+				echo "$RIG_NUM:Not Applicable:Not Applicable"
+				;;
+			*)
+				Message "rigctld is running. Rig # ${RIG_NUM} ${MAKE_MODEL} on ${DEVICE} at $SPEED"
+				echo "$RIG_NUM:${DEVICE##*/}:$SPEED"
+				;;
+		esac
+	else
+		Message "rigctld is not running."
+		echo ""
+	fi
 }
+export -f rigctldStatus
+
+function restartRigctld () {
+	source $RIGS_FILE
+	local RIG_NUM=$(declare -p RIGS_ARRAY | sed -n "s|.*\[\(.*\)\]=\"${1}\".*|\1|p")
+	if ! [[ $RIG_NUM =~ ^[0-9] ]]
+	then
+		Message "ERROR: Invalid Rig: ${1}"
+		return 1
+	fi
+	case $RIG_NUM in
+		1|2|4|6) # Hamlib dummy and FLRig "rigs" don't use a serial port
+			PORT=""
+			SPEED=""
+			Message "Serial port, speed not needed. Setting to Not Applicable"
+			;;
+		*)
+			if [[ ${3} =~ ^Not || ${4} =~ ^Not ]]
+			then
+				Message "ERROR: Rig ${1} requries a serial port and speed"
+				return 1
+			fi
+			#PORT="-r /dev/serial/by-id/${2}"
+			# Strip out any newlines inserted when the yad combobox entries were created
+			PORT="-r $(tr -d '\n' <<<${2})"
+			SPEED="-s ${3}"
+			;;
+	esac
+	eval $(grep RIGCTLD_PORT $ARGS_CONFIG)
+	if [[ -n "$RIGCTLD_PORT" ]]
+	then
+		argModify RIGCTLD_ARGS=\"-v -t $RIGCTLD_PORT -m $RIG_NUM $PORT $SPEED\"
+	else
+		argModify RIGCTLD_ARGS=\"-v -m $RIG_NUM $PORT $SPEED\"
+	fi
+	eval $(grep RIGCTLD_ARGS $ARGS_CONFIG)
+	stopRigctld
+	if systemctl --user start $(systemd-escape --template rigctld@.service "$ARGS_CONFIG")
+	then
+		Message "rigctld $RIGCTLD_ARGS for $1 started successfully."
+	else
+		Message "ERROR starting rigctld $RIGCTLD_ARGS for $1"
+		return 1
+	fi
+	return 0    		
+}
+export -f restartRigctld
+
+function stopRigctld () {
+	systemctl --user stop $(systemd-escape --template rigctld@.service "$ARGS_CONFIG")
+	Message "rigctld stopped."
+}
+export -f stopRigctld
 
 function getSerialPorts() {
 	# Returns '|' list of the basenames of all files /dev/serial/by-id 
 	PORTS="Not Applicable|"
-	for P in /dev/serial/by-id/*
+	local MAXLEN=60
+	for P in /dev/tty[US]* $(find /dev -lname "*tty[US]*")
 	do
-		PORTS+="$(basename "$P")|"
+   	if ! [[ $P =~ /dev/char ]]
+   	then
+   		# Tag the default port
+  			[[ -n $1 && $P =~ $1 ]] && P="^$P"
+   		# Wrap if lines exceed MAXLEN so that GUI doesn't get too long
+   		if [[ ${#P} -gt $MAXLEN ]]
+   		then
+  				P="$(sed -r "s/.{$MAXLEN}/&\n/g" <<<$P)"
+   		fi
+   		PORTS+="$P|"
+		fi
 	done
-	[[ $1 != "" && $PORTS =~ $1 ]] && echo "$PORTS" | sed -e "s/$1/^$1/" -e 's/|$//' || echo "$PORTS" | sed -e 's/|$//'
+	echo -e "$PORTS" | sed -e 's/|$//'
 }
 
 function getSpeeds() {
 	# Returns '|' list of serial port speeds
 	SPEEDs="Not Applicable|300|1200|2400|4800|9600|19200|38400|57600|115200"
 	[[ $1 != "" && $SPEEDs =~ $1 ]] && echo "$SPEEDs" | sed -e "s/$1/^$1/" || echo "$SPEEDs"
-}
-
-function loadSettings () {
-	if [ -s "$CONFIG_FILE" ]
-	then # There is a config file
-   	#echo "$CONFIG_FILE found." >&3
-  		source "$CONFIG_FILE"
-	else # Set some default values in a new config file
-   	#echo -e "Config file $CONFIG_FILE not found.\nCreating a new one with default values." >&3
-		setrigctldDefaults
-   	echo "declare -gA F" > "$CONFIG_FILE"
-   	echo "F[_RIG_]='${D[1]}'" >> "$CONFIG_FILE"
-   	echo "F[_PORT_]='${D[2]}'" >> "$CONFIG_FILE"
-   	echo "F[_SPEED_]='${D[3]}'" >> "$CONFIG_FILE"
-   	source "$CONFIG_FILE"
-	fi
 }
 
 #============================
@@ -204,26 +254,20 @@ SCRIPT_ID="$(ScriptInfo | grep script_id | tr -s ' ' | cut -d' ' -f3)"
 SCRIPT_HEADSIZE=$(grep -sn "^# END_OF_HEADER" ${0} | head -1 | cut -f1 -d:)
 VERSION="$(ScriptInfo version | grep version | tr -s ' ' | cut -d' ' -f 4)"
 
-TITLE="Hamlib Rig Control (rigctld) Configuration $VERSION"
-CONFIG_DIR="$HOME/.config/nexus"
+TITLE="Hamlib Rig Control (rigctld) Manager $VERSION"
+CONFIG_DIR="$TMPDIR"
 [[ -f "$HOME/rigctld.conf" ]] && mv "$HOME/rigctld.conf" "$CONFIG_DIR/"
 CONFIG_FILE="$CONFIG_DIR/rigctld.conf"
+DEFAULT_ARGS_CONF="$HOME/.config/nexus/args.conf"
 MESSAGE="Hamlib rigctld Configuration"
 
-#PAT_VERSION="$(pat version | cut -d' ' -f2)"
-#[[ $PAT_VERSION =~ v0.1[01]. ]] && PAT_CONFIG="$HOME/.wl2k/config.json" || 
-PAT_CONFIG="$HOME/.config/pat/config.json"
-[[ -f "$HOME/.wl2k/config.json" ]] && mv "$HOME/.wl2k/config.json" "$PAT_CONFIG"
-export PAT_CONFIG=$PAT_CONFIG
-export find_cmd='@bash -c "runFind "%1""'
-export view_remove_cmd='bash -c "viewDeleteAliases"'
-export fpipe=$(mktemp -u --tmpdir find.XXXXXXXX)
-mkfifo "$fpipe"
-DEFAULT_SEARCH_STRING="$(jq -r .locator $PAT_CONFIG)"
+TERM_PIPE=$TMPDIR/termpipe
+mkfifo $TERM_PIPE
+exec 5<> $TERM_PIPE
+export RIGCTL_LOG="$TMPDIR/rigctld.log"
+touch $RIGCTL_LOG
+export RIGS_FILE="$TMPDIR/rigs"
 fkey=$(($RANDOM * $$))
-YAD_PIDs=()
-
-exec 4<> $fpipe
 
 #============================
 #  PARSE OPTIONS WITH GETOPTS
@@ -270,7 +314,8 @@ do
 	fi
 
 	# Options followed by another option instead of argument
-	if [[ "x${OPTION}" != "x:" ]] && [[ "x${OPTION}" != "x?" ]] && [[ "${OPTARG}" = -* ]]; then 
+	if [[ "x${OPTION}" != "x:" ]] && [[ "x${OPTION}" != "x?" ]] && [[ "${OPTARG}" = -* ]]
+	then 
 		OPTARG="$OPTION" OPTION=":"
 	fi
 
@@ -303,7 +348,12 @@ do
 	command -v $A >/dev/null 2>&1 || Die "$A is required but not installed."
 done
 
-RESTART_RIGCTLD=FALSE
+export ARGS_CONFIG=${1:-$DEFAULT_ARGS_CONF}
+touch $ARGS_CONFIG
+#cat $ARGS_CONFIG
+[[ -f $ARGS_CONFIG ]] || SafeExit 1
+#SOCAT_PORT=${2:-3333}
+#argModify SOCAT_PORT=$SOCAT_PORT
 
 #============================
 #  MAIN SCRIPT
@@ -311,132 +361,71 @@ RESTART_RIGCTLD=FALSE
 
 trap SafeExit INT TERM EXIT
 
-while true
+declare -A RIGS_ARRAY
+RIGS=""
+while read -r LINE
 do
-	# Kill any running processes and load latest settings
-   for P in ${YAD_PIDs[@]}
-	do
-		ps x | egrep -q "^$P" && kill $P
-	done
-	loadSettings
-	YAD_PIDs=()
-	if [[ $RESTART_RIGCTLD == TRUE ]]
-	then # Restart requested 
-		RIG="${F[_RIG_]}"
-		[[ $RIG =~ ^[0-9] ]] || Die "Invalid Rig: ${F[_RIG_]}"
-		[[ ${F[_PORT_]} =~ ^Not ]] && PORT="" || PORT="-r /dev/serial/by-id/${F[_PORT_]}"
-		[[ ${F[_SPEED_]} =~ ^Not ]] && SPEED="" || SPEED="-s ${F[_SPEED_]}"
-		COM="$(command -v rigctld) -m ${RIG%%|*} $PORT $SPEED"
-		$(command -v rigctld) -m ${RIG%%|*} $PORT $SPEED 2>$TMPDIR/rigctld_error.txt &
-   	rigctld_PID=$!
-		if ! pgrep -x rigctld >/dev/null && [[ -s $TMPDIR/rigctld_error.txt ]]
-		then
-			yad --center --title="Hamlib rigctld ERROR" --text-align=center --borders=30 --width=600 --text-wrap \
-				--text="<big><b>$COM\n<span color='red'>failed!</span></b>\nError message:</big>\n" --buttons-layout=center \
-				--text-info <$TMPDIR/rigctld_error.txt \
-				--button=Close:0 >/dev/null
-		fi
-		rm -f $TMPDIR/rigctld_error.txt
-		RESTART_RIGCTLD=FALSE
-	fi
-	if pgrep -x rigctld >/dev/null
-	then # rigctld already running
-		# Determine rig ID of running rigctld process
-		ID="$(cat /proc/$(pidof rigctld)/cmdline | tr -d '\0' | tr '-' '\n' | egrep "^m[0-9]{1,4}" | sed -e 's/^m//')"
-		case $ID in
-			1|2|4) # Hamlib and FLRig "rigs" don't use a serial port
-				PORTSPEED_MSG=""
-				;;
-	 		*) # All(?) of the others require a serial port
-				PORT="$(cat /proc/$(pidof rigctld)/cmdline | tr '\0' '~' | sed -e 's/~-/\n-/g' | tr '~' ' ' | grep "^-r" | cut -d' ' -f2)"
-				[[ $PORT == "" ]] && PORT="PORT NOT CONFIGURED"
-				SPEED="$(cat /proc/$(pidof rigctld)/cmdline | tr '\0' '~' | sed -e 's/~-/\n-/g' | tr '~' ' ' | grep "^-s" | cut -d' ' -f2)"
-				[[ $SPEED == "" ]] && SPEED="SPEED NOT CONFIGURED"
-				PORTSPEED_MSG="on ${PORT##*/} @ $SPEED"
-				;;
-		esac
-		# Find the rig make and model from that ID
-		RIG="$(getRig $ID)"
-		STATE="<b><span color='green'><big>rigctld is RUNNING for $RIG</big>\n$PORTSPEED_MSG</span></b> \n \
-<b>Change the rig by searching/selecting a different rig below, or click 'Save...' to restart rigctld with $RIG</b>"
-	else # rigctld not running
-		RIG="$(echo ${F[_RIG_]} | cut -d'|' -f2,3 | tr '|' ' ')"
-		STATE="<big><b><span color='red'>rigctld is NOT RUNNING</span></b></big>\n<b>Configured rig is <span color='blue'>$RIG.</span> Change the rig by searching/selecting a different rig below, or click 'Save...' to start rigctld with $RIG</b>"
-	fi
-	yad --plug="$fkey" --tabnum=1 --text-align=center \
-	   --text="<big><b>Configure Hamlib Rig Control (rigctld)</b></big>\n$STATE\n \
-<span color='red'><b>Not all rigs are supported by Hamlib!</b></span> Search for your rig using regex (case insensitive).\n \
-Example: Find all Kenwood rigs with model numbers staring with 'TM': <b>kenwood.*TM</b>\n \
-Hamlib and FLRig models don't use the Serial Port or Speed settings.  Set them to 'Not Applicable' for those models." \
-	 	--form \
-  	 	--align=right \
-		--columns=2 \
-  	 	--item-separator="|" \
-	 	--field="<b>Rig search string</b>" "" \
- 	--field="Find":FBTN "$find_cmd" \
-	 	--field="<b>Serial Port</b>":CB "$(getSerialPorts "${F[_PORT_]}")" \
-	 	--field="<b>Speed</b>":CB "$(getSpeeds "${F[_SPEED_]}")" >$TMPDIR/RIG_PARAMS.txt &
-	YAD_PIDs+=( $! )
+   INDEX="$(cut -d' ' -f1 <<<"$LINE")"
+   VALUE="$(cut -d' ' -f2- <<<"$LINE")"
+   RIGS_ARRAY[$INDEX]=$VALUE
+   RIGS+="${VALUE}|"
+done < <($(command -v rigctl) -l | grep -v '^ Rig' | \
+   cut -c-55 | sed -e 's/^ *//' -e 's/ *$//' | \
+   tr -s '[:space:]')
+RIGS="$(echo "$RIGS" | sed "s/|$//")"
+declare -p RIGS_ARRAY > $RIGS_FILE
 
-	yad --plug="$fkey" --tabnum=2 --list --grid-lines=hor \
-		--text-align=left \
-		--text "Rig search results. <span color='blue'><b>Select new rig from the list below, select Serial Port and Speed above, then click 'Save...' below to restart rigctld.</b></span>" \
-		--radiolist --column="Pick" --column="ID" --column="Make" --column="Model" \
-		--expand-column=4 <&4 >$TMPDIR/RIG_SELECTION.txt &
-	YAD_PIDs+=( $! )
+RUNNING_RIG=$(rigctldStatus)
+if [[ -n $RUNNING_RIG ]]
+then
+	INDEX=$(cut -d: -f1 <<<"$RUNNING_RIG")
+	DEFAULT_RIG="${RIGS_ARRAY[$INDEX]}"
+	RIGS="$(echo "$RIGS" | sed -e "s/$DEFAULT_RIG/^$DEFAULT_RIG/")"
+	DEFAULT_DEVICE="$(cut -d: -f2 <<<"$RUNNING_RIG")"
+	DEFAULT_SPEED="$(cut -d: -f3 <<<"$RUNNING_RIG")"
+fi
 
-	yad --paned --key="$fkey" --buttons-layout=center \
-  		--borders=20 \
-		--geometry=600x600+10+50 \
-		--title="$TITLE" --window-icon="system-search" \
-		--button="<b>Leave rigctld as-is &#x26; Exit</b>":3 \
-		--button="<b>Stop rigctld &#x26; Exit</b>":2 \
-		--button="<b>Stop rigctld</b>":1 \
-		--button="<b>Save Changes &#x26; [Re]start rigctld</b>":0
-	RETURN_CODE=$?	
-	YAD_PIDs+=( $RETURN_CODE )
-	
-	case $RETURN_CODE in
-		0) # Read and handle the Configure TNC tab yad output
-			[[ -s $TMPDIR/RIG_PARAMS.txt ]] || Die "Unexpected input from dialog via $TMPDIR/RIG_PARAMS.txt"
-			IFS='|' read -r -a PARAMS < "$TMPDIR/RIG_PARAMS.txt"
-			F[_PORT_]="${PARAMS[2]}"
-			F[_SPEED_]="${PARAMS[3]}"
-			PARAMS=()
-			if [[ -s $TMPDIR/RIG_SELECTION.txt ]]
-			then
-				IFS='|' read -r -a PARAMS < "$TMPDIR/RIG_SELECTION.txt"
-				F[_RIG_]="${PARAMS[1]}|${PARAMS[2]}|${PARAMS[3]}"
-			fi
-			RIG="${F[_RIG_]}"
-			case ${RIG%%|*} in
-				1|2|4) # Hamlib and FLRig "rigs" don't use a serial port
-					F[_PORT_]="Not Applicable"
-					F[_SPEED_]="Not Applicable"
-					;;
-		 		*) # All(?) of the others require a serial port
-					;;
-			esac
+YAD_PIDs=()
 
-			# Update the yad configuration file.
-			echo "declare -gA F" > "$CONFIG_FILE"
-			for J in "${!F[@]}"
-			do
-   			echo "F[$J]='${F[$J]}'" >> "$CONFIG_FILE"
-			done
-			pkill -x rigctld
-			RESTART_RIGCTLD=TRUE
-			;;
-		1) # Stop rigctld
-			pkill -x rigctld
-			;;
-		2) # Stop rigctld and exit
-			pkill -x rigctld
-			break
-			;;
-		*) # Leave rigctld as-is and exit 
-			break
-			;;
-	esac
-done
-SafeExit
+yad --plug="$fkey" --tabnum=1 --text-align=center \
+    --text="<big><b>Manage Hamlib Rig Control Daemon (rigctld)</b></big>\n \
+   <span color='red'><b>Not all rigs are supported by Hamlib!</b></span>\nStart typing \
+the make/model of radio in the <b>Select Rig</b> field below then select \
+rig from the list. Hamlib and FLRig models don't use the <b>Serial Port</b> or \
+<b>Speed</b> settings. Set them to 'Not Applicable' for those models." \
+  	--form \
+  	--align=right \
+ 	--columns=2 \
+ 	--css="$HOME/.config/gtk-3.0/yad.css" \
+ 	--complete=any \
+  	--item-separator="|" \
+  	--focus-field=1 \
+  	--field="<b>Select Rig</b>":CE "$RIGS" \
+  	--field="<b>Serial Port</b>":CB "$(getSerialPorts "$DEFAULT_DEVICE")" \
+  	--field="<b>Speed</b>":CB "$(getSpeeds "$DEFAULT_SPEED")" \
+  	--field="<b>[Re]start rigctld</b>":FBTN 'bash -c "restartRigctld %1 %2 %3; rigctldStatus >/dev/null"' \
+  	--field="<b>Stop rigctld</b>":FBTN 'bash -c "stopRigctld"' \
+  	--field="<b>Show rigctld status</b>":FBTN 'bash -c "rigctldStatus >/dev/null"' &
+YAD_PIDs+=( $! )
+   
+yad --plug="$fkey" --tabnum=2 --text-align="center" \
+	 --back=black --fore=yellow --text-info \
+	 --tail --listen <&5 &
+monitor_PID=$!
+YAD_PIDs+=( $monitor_PID )
+(tail -F --pid=$monitor_PID -q -n 5 $RIGCTL_LOG 2>/dev/null | cat -v >&5) &
+
+yad --paned --key="$fkey" --buttons-layout=center \
+  	--borders=10 \
+  	--geometry=450x450+10+50 \
+  	--focused=1 \
+  	--title="$TITLE" \
+  	--button="<b>Close</b>"!!"Close this manager, leaving rigctld as-is":1
+RETURN_CODE=$?	
+   	
+case $RETURN_CODE in
+  	*) # exit
+  		SafeExit 0
+  		;;
+esac
+SafeExit 0
